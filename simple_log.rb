a76f852ec9829a90bf6c5f15c3eb6c2767717eca
Xia Xiongjun <xxjapp@gmail.com>
2014-07-02 02:18:03 +0900

    add file

---
+++ simple_log.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/env ruby
+# encoding: UTF-8
+#
+# - module SimpleLog
+#
+
+require 'logger'
+
+class SimpleLog < Logger
+    def initialize(logdev, shift_age = 0, shift_size = 1048576)
+        super(logdev, shift_age, shift_size)
+
+        self.formatter = proc do |severity, datetime, progname, msg|
+            file, line, method = caller[4].match(%r[.*/(.*):(\d+):in `(.*)']).captures
+            "#{datetime.to_s[0..-7]} #{severity[0]} (#{file}:#{line}) #{method} - #{msg}\n"
+        end
+    end
+end
+
+if __FILE__ == $0
+    LOG = SimpleLog.new(STDOUT)
+
+    def func
+        LOG.info("info")
+    end
+
+    func
+    LOG.fatal("fatal")
+end
